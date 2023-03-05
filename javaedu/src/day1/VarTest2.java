79b18c07d1efb5136c59bede8fe1862e631fd52f
unico67 <unicodaum@hanmail.net>
2023-03-05 15:06:58 +0900

    1일차 수업내용

---
+++ javaedu/src/day1/VarTest2.java
@@ -0,0 +1,27 @@
+package day1;
+public class VarTest2 {
+	public static void main(String[] args) {
+		System.out.println(100);
+		int num;  
+		num = 1000;
+		System.out.println("연산 전 : "+num);
+		num = 10+20;
+		System.out.println("연산 후 : "+num);
+		char munja = 'A';  
+		System.out.print(munja); 
+		munja++;
+		System.out.print(munja); 
+		munja += 1;
+		System.out.print(munja); 
+		munja = (char)(munja+1); 
+		System.out.println(munja);  
+	}
+}
+
+
+
+
+
+
+
+
