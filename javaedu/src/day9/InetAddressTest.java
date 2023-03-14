99df900276c73e574c13565aab4dfff7c20b59dd
unico67 <unicodaum@hanmail.net>
2023-03-14 22:13:07 +0900

    8일차 수업준비

---
+++ javaedu/src/day9/InetAddressTest.java
@@ -0,0 +1,10 @@
+package day9;
+import java.net.*;
+public class InetAddressTest {
+	public static void main(String[] args) throws Exception {
+		InetAddress inet = InetAddress.getLocalHost();
+		System.out.println(inet);
+		inet = InetAddress.getByName("www.naver.com");
+		System.out.println(inet);
+	}
+}
